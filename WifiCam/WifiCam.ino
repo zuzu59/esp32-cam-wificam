eb353332d73aef8c69f2c415870046a4e0dc6fc2
Christian Zufferey <christian@zufferey.com>
2024-09-25 16:41:24 +0200

    change: changé le nom de la caméra pour le msl

--- WifiCam/WifiCam.ino
+++ WifiCam/WifiCam.ino
@@ -3,10 +3,12 @@
 // ATTENTION, ce code a été testé sur une Ai Thinker ESP32-CAM. Pas testé sur les autres boards !
 // Initial commit zf231111
 //
-#define zVERSION        "zf240925.1610"
+#define zVERSION        "zf240925.1622"
 // Il faut aussi modifier 'zWifiVersion' dans handlers.cpp !
 //#define zHOST           "esp-cam-st-luc1"        // ATTENTION, tout en minuscule
-#define zHOST           "esp-cam-crissier1"        // ATTENTION, tout en minuscule
+// #define zHOST           "esp-cam-crissier1"        // ATTENTION, tout en minuscule
+// #define zHOST           "esp-cam-msl1"        // ATTENTION, tout en minuscule
+#define zHOST           "esp-cam-msl2"        // ATTENTION, tout en minuscule
 // #define zIpStatic
 // IPAddress zLocal_IP(192, 168, 57, 61);
 // IPAddress zGateway(192, 168, 57, 1);
