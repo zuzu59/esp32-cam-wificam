e3401d0176c8ce37452c0a8db02c19b5df7b8984
Christian Zufferey <christian@zufferey.com>
2025-03-25 22:18:28 +0100

    wip

--- WifiCam/WifiCam.ino
+++ WifiCam/WifiCam.ino
@@ -3,7 +3,7 @@
 // ATTENTION, ce code a été testé sur une Ai Thinker ESP32-CAM. Pas testé sur les autres boards !
 // Initial commit zf231111
 //
-#define zVERSION        "zf250325.2131"
+#define zVERSION        "zf250325.2215"
 // Il faut aussi modifier 'zWifiVersion' dans handlers.cpp !
 // #define zHOST           "esp-cam-st-luc1"          // ATTENTION, tout en minuscule
 #define zHOST           "esp-cam-st-luc-cuisine"          // ATTENTION, tout en minuscule
